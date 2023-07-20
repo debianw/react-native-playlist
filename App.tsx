d5f0714571100814250cfb3ce6986f619e6376df
Waly <debianw@gmail.com>
2023-07-20 17:32:44 -0600

    Updading text

--- App.tsx
+++ App.tsx
@@ -8,16 +8,31 @@ const lessonsPlayList: VideoItem[] = [
     id: '1',
     source: require('./assets/lessons/step1.mp4'),
     title: 'Handling',
+    shortDescription:
+      'If you want to master a habit, the key is to start with repetition, not perfection.',
+    longDescription: `If you want to master a habit, the key is to start with repetition, not perfection.
+      More detailed text could be here if needed. The video would stop when this comes up and when closed would resume if video was playing.
+      The idea is the text expands up and could reach the top of the screen, and the video becomes translucent to black. Pressing play would also close this text as would swiping down.`,
   },
   {
     id: '2',
     source: require('./assets/lessons/step2.mp4'),
     title: 'Handling',
+    shortDescription:
+      'If you want to master a habit, the key is to start with repetition, not perfection.',
+    longDescription: `If you want to master a habit, the key is to start with repetition, not perfection.
+      More detailed text could be here if needed. The video would stop when this comes up and when closed would resume if video was playing.
+      The idea is the text expands up and could reach the top of the screen, and the video becomes translucent to black. Pressing play would also close this text as would swiping down.`,
   },
   {
     id: '3',
     source: require('./assets/lessons/step3.mp4'),
     title: 'Handling',
+    shortDescription:
+      'If you want to master a habit, the key is to start with repetition, not perfection.',
+    longDescription: `If you want to master a habit, the key is to start with repetition, not perfection.
+      More detailed text could be here if needed. The video would stop when this comes up and when closed would resume if video was playing.
+      The idea is the text expands up and could reach the top of the screen, and the video becomes translucent to black. Pressing play would also close this text as would swiping down.`,
   },
   {
     id: '4',
